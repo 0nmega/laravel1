89a0c519f804ad1632ad1f20020994b32c2210c4
0nmega <0nmega123@gmail.com>
2024-10-07 13:04:11 +0300

    5

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,8 @@ import './bootstrap';
 import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 Alpine.start();
+const mix = require('laravel-mix');
+
+mix.js('resources/js/app.js', 'public/js')
+    .css('resources/css/app.css', 'public/css')
+    .version(); //
